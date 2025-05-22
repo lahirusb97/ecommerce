ba8c7c970f6bbf54dc0cf433e9f2075c26e79492
lahirusb97 <lahirushirandev@gmail.com>
2025-05-22 10:44:07 +0530

    s

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["res.cloudinary.com", "www.hollywoodreporter.com"],
+    domains: [
+      "res.cloudinary.com",
+      "www.hollywoodreporter.com",
+      "assets.aceternity.com",
+    ],
     remotePatterns: [
       {
         protocol: "https",
